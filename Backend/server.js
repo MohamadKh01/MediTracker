d03a83de69c09e5b7da6c91a782bdf4d863c74cf
mohamad khatib <mohamadkh6324@gmail.com>
2026-02-22 13:43:32 +0200

    Chore: initialize backend server with express and mongoDB

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
 
+// import database connection function
+const connectDB = require('./config/db.js')
+
+// load .env file content into process.env
+dotenv.config();
+
+//connect to mongoDB
+connectDB();
+
+// middleware
 const app = express();
+app.use(cors());
 app.use(express.json());
 
+// basic route
 app.get('/', (req, res) => {
-    res.send('API is running...');
-})
+    res.send('MediTracker API is running...');
+});
 
-app.listen(3000, () => {
-    console.log("app listening on http://localhost:3000");
-})
+app.listen(process.env.PORT, () => {
+    console.log(`app listening on http://localhost:${process.env.PORT}`);
+});
