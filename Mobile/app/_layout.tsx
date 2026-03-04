c46a3a62c3a35c53ed8f829b5531e368dd03a588
mohamad khatib <mohamadkh6324@gmail.com>
2026-03-04 16:45:32 +0200

    Bug: fixed keyboard not closing bug

--- Mobile/app/_layout.tsx
+++ Mobile/app/_layout.tsx
@@ -1,5 +1,12 @@
 import { Stack } from "expo-router";
+import { KeyboardAvoidingView, Platform, TouchableWithoutFeedback, Keyboard, View } from "react-native";
 
 export default function Layout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <KeyboardAvoidingView style={{ flex: 1}} behavior={Platform.OS === "ios" ? "padding" : "height"} keyboardVerticalOffset={Platform.OS === "ios" ? 0 : 20} >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Stack screenOptions={{ headerShown: false }} />
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
 }
