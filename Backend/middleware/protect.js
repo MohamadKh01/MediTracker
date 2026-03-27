73e6f47b781101890b8b81e22f8a59a2b747023f
mohamad khatib <mohamadkh6324@gmail.com>
2026-03-27 20:12:14 +0200

    Bug: fixed a typo

--- Backend/middleware/protect.js
+++ Backend/middleware/protect.js
@@ -5,31 +5,31 @@ const protect = async (req, res, next) => {
     try {
         let token;
 
-        if(req.headers.authotization && req.headers.authorization.startsWith('Bearer')) {
+        if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
             token = req.headers.authorization.split(' ')[1];
         }
 
-        if(!token) {
-            return res.status(401).json({ message: 'Not authorized, no token'});
+        if (!token) {
+            return res.status(401).json({ message: 'Not authorized, no token' });
         }
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-        req.user = await user.findById(decoded.id).select('-password');
+        req.user = await User.findById(decoded.id).select('-password');
 
         next();
     } catch (err) {
-        return res.status(401).json({ message: 'Not authorized, Token failed'});
+        return res.status(401).json({ message: 'Not authorized, Token failed' });
     }
 };
 
 const authorize = async (...roles) => {
     return (req, res, next) => {
-        if(!roles.includes(req.user.role)) {
-            return res.status(403).json({ message: `Access denied: role ${req.user.role} not authorized`});
+        if (!roles.includes(req.user.role)) {
+            return res.status(403).json({ message: `Access denied: role ${req.user.role} not authorized` });
         }
         next();
     };
 };
 
-module.exports = { protect, authorize};
+module.exports = { protect, authorize };
